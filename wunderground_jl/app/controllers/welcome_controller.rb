ae8ba88220c10e8a4db744161f82cce35ef2d817
Joseph Lombardo <me@joseph-lombardo.com>
2017-11-13 20:53:36 -0500

    Adding in wunderground test app

---
+++ wunderground_jl/app/controllers/welcome_controller.rb
@@ -0,0 +1,63 @@
+class WelcomeController < ApplicationController
+  def test
+    response = HTTParty.get("http://api.wunderground.com/api/7de47b5fbbe6262b/geolookup/conditions/q/AZ/Phoenix.json")
+	  
+	  @location = response['location']['city']
+	  @temp_f = response['current_observation']['temp_f']
+	  @temp_c = response['current_observation']['temp_c']
+	  @weather_icon = response['current_observation']['icon_url']
+	  @weather_words = response['current_observation']['weather'] 
+	  @forecast_link = response['current_observation']['forecast_url']
+	  @real_feel = response['current_observation']['feelslike_f']
+  end
+
+  def index
+			# Creates an array of states that our user can choose from on our index page
+			@states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK 
+				TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY 
+				NJ CT RI MA VT NH ME DC).sort!
+	
+			location_exists = false
+			Location.all.each do |location|
+				if location.city == params[:city] && location.state == params[:state]
+					location_exists = true
+				end
+			end
+
+			# removes spaces from the 2-word city names and replaces the space with an underscore 
+			if params[:city] != nil
+					params[:city].gsub!(" ", "_")
+			end
+	
+			#checks that the state and city params are not empty before calling the API
+			if params[:state] != "" && params[:city] != "" && params[:state] != nil && params[:city] != nil
+				results = HTTParty.get("http://api.wunderground.com/api/#{Figaro.env.wunderground_api_key}/geolookup/conditions/q/#{params[:state]}/#{params[:city]}.json")
+				#if no error is returned from the call, we fill our instants variables with the result of the call
+				if results['response']['error'] == nil || results['error'] == ""
+					#Checks to see if the response contains an array (ambigous response for an invalid city/state combination) or a hash(valid response)
+					if results.key?("location") 
+						# Store our location in the database if we don't already have it
+						if location_exists == false
+							dname = params[:city] + ", " + params[:state]
+							Location.create(city: params[:city], state: params[:state], dname: dname)
+						end
+						@location = results['location']['city']
+						@temp_f = results['current_observation']['temp_f']
+						@temp_c = results['current_observation']['temp_c']
+						@weather_icon = results['current_observation']['icon_url']
+						@weather_words = results['current_observation']['weather']
+						@forecast_link = results['current_observation']['forecast_url']
+						@real_feel_f = results['current_observation']['feelslike_f']
+						@real_feel_c = results['current_observation']['feelslike_c']
+					else
+						@error = "City/State combination does not exist." 
+					end		
+				else
+					# if there is an error, we report it to our user via the @error variable 	
+					@error = results['response']['error']['description']
+				end
+			end
+			@stored_locations = Location.order(:city)		
+		end
+
+end
